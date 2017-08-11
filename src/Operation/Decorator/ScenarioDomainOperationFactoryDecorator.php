9ec6822771dee48eaf559534544c89d421d15188
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-11 12:41:03 +0300

    Added DomainScenarioFactory

--- src/Operation/Decorator/ScenarioDomainOperationFactoryDecorator.php
+++ src/Operation/Decorator/ScenarioDomainOperationFactoryDecorator.php
@@ -15,6 +15,7 @@ namespace Vainyl\Domain\Operation\Decorator;
 use Vainyl\Domain\DomainInterface;
 use Vainyl\Domain\Operation\Factory\DomainOperationFactoryInterface;
 use Vainyl\Domain\Scenario\CheckScenarioOperation;
+use Vainyl\Domain\Scenario\Factory\ScenarioFactoryInterface;
 use Vainyl\Domain\Scenario\Storage\DomainScenarioStorageInterface;
 use Vainyl\Operation\Collection\Factory\CollectionFactoryInterface;
 use Vainyl\Operation\OperationInterface;
@@ -30,20 +31,26 @@ class ScenarioDomainOperationFactoryDecorator extends AbstractDomainOperationFac
 
     private $scenarioStorage;
 
+    private $scenarioFactory;
+
     /**
      * ScenarioDomainOperationFactoryDecorator constructor.
      *
      * @param DomainOperationFactoryInterface $operationFactory
      * @param CollectionFactoryInterface      $collectionFactory
-     * @param DomainScenarioStorageInterface  $domainStorage
+     * @param DomainScenarioStorageInterface  $scenarioStorage
+     * @param ScenarioFactoryInterface        $scenarioFactory
      */
     public function __construct(
         DomainOperationFactoryInterface $operationFactory,
         CollectionFactoryInterface $collectionFactory,
-        DomainScenarioStorageInterface $domainStorage
+        DomainScenarioStorageInterface $scenarioStorage,
+        ScenarioFactoryInterface $scenarioFactory
+
     ) {
         $this->collectionFactory = $collectionFactory;
-        $this->scenarioStorage = $domainStorage;
+        $this->scenarioStorage = $scenarioStorage;
+        $this->scenarioFactory = $scenarioFactory;
         parent::__construct($operationFactory);
     }
 
@@ -53,8 +60,10 @@ class ScenarioDomainOperationFactoryDecorator extends AbstractDomainOperationFac
     public function create(DomainInterface $domain): OperationInterface
     {
         $collection = $this->collectionFactory->create();;
-        foreach ($this->scenarioStorage->getScenarios(get_class($domain)) as $scenario) {
-            $collection->add(new CheckScenarioOperation($domain, $scenario));
+        foreach ($this->scenarioStorage->getScenarios(get_class($domain)) as $name => $settings) {
+            $collection->add(
+                new CheckScenarioOperation($domain, $this->scenarioFactory->createScenario($name, $settings))
+            );
         }
 
         return $collection->add(parent::create($domain));
@@ -66,8 +75,10 @@ class ScenarioDomainOperationFactoryDecorator extends AbstractDomainOperationFac
     public function update(DomainInterface $newDomain, DomainInterface $oldDomain): OperationInterface
     {
         $collection = $this->collectionFactory->create();;
-        foreach ($this->scenarioStorage->getScenarios(get_class($newDomain)) as $scenario) {
-            $collection->add(new CheckScenarioOperation($newDomain, $scenario));
+        foreach ($this->scenarioStorage->getScenarios(get_class($newDomain)) as $name => $settings) {
+            $collection->add(
+                new CheckScenarioOperation($newDomain, $this->scenarioFactory->createScenario($name, $settings))
+            );
         }
 
         return $collection->add(parent::update($newDomain, $oldDomain));
@@ -79,8 +90,10 @@ class ScenarioDomainOperationFactoryDecorator extends AbstractDomainOperationFac
     public function upsert(DomainInterface $domain): OperationInterface
     {
         $collection = $this->collectionFactory->create();;
-        foreach ($this->scenarioStorage->getScenarios(get_class($domain)) as $scenario) {
-            $collection->add(new CheckScenarioOperation($domain, $scenario));
+        foreach ($this->scenarioStorage->getScenarios(get_class($domain)) as $name => $settings) {
+            $collection->add(
+                new CheckScenarioOperation($domain, $this->scenarioFactory->createScenario($name, $settings))
+            );
         }
 
         return $collection->add(parent::upsert($domain));
